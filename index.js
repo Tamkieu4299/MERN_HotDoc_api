d76e739d2962934923a8d4337b80f78d07dbe73b
Nguyen Tuan Dat <datb9a2@gmail.com>
2022-09-05 11:19:57 +0700

    adding chat

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ const medicineRoute = require("./routes/medicines");
 const authRoute = require("./routes/auth");
 const reviewRoute = require("./routes/reviews");
 
+const { chats } = require("./data/data")
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+
 // Express Application
-const app = express();
 dotenv.config();
+
+const app = express();
 
 // Database connection
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
@@ -28,6 +35,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+app.get('/', (req, res) => {
+    res.send("API is Running");
+});
 
 // Routes
 app.use("/api/doctors", doctorRoute);
@@ -36,8 +46,54 @@ app.use("/api/appointments", appointmentRoute);
 app.use("/api/medicines", medicineRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/reviews", reviewRoute);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
-// Run on port
-app.listen(8800, () => {
-    console.log("Backend is running");
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8800
+
+const server = app.listen(8800, console.log(`Backend Server Started on PORT ${PORT}`));
+
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+    console.log('connected to socket.io');
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit('connected');
+    })
+    
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log("user joined room: " + room);
+    });
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageRecieved.sender._id) return;
+
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    });
 });
--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,12 @@
 const customerController = require("../controllers/customerController");
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddlewareDoctor")
 
 // Register a customer
-router.post("/register", customerController.registerCustomer)
+router.route("/register").post(customerController.registerCustomer).get(protect,customerController.searchAllCustomers);
 // Login with customer
-// router.post("/login", customerController.loginCustomer);
+router.post("/login", customerController.authCustomer);
 // Update a customer
 router.put("/:id", customerController.updateCustomer);
 // Get a customer
